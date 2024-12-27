80fe296fb9c3fb123fc7cddb874520982695906d
Alison Bento <alisonbnt@gmail.com>
2024-12-27 19:51:04 -0300

    add missing context for editor module

--- lua/apollo/editor.lua
+++ lua/apollo/editor.lua
@@ -1,19 +1,20 @@
 local M = {}
 
 M.load = function(ctx)
+	local settings = require('apollo.context').settings.editor
+
 	-- General settings
 	vim.wo.relativenumber = true
 	vim.wo.number = true
 	vim.wo.signcolumn = 'yes'
-	vim.wo.cursorline = ctx.settings.editor.cursorline
-	vim.opt.colorcolumn = ctx.settings.editor.colorcolumn
+	vim.wo.cursorline = settings.cursorline
+	vim.opt.colorcolumn = settings.colorcolumn
 
 	-- Indentation
-	local indent_size = ctx.settings.editor.indent_size
-	vim.bo.shiftwidth = indent_size
+	vim.bo.shiftwidth = settings.indent_size
 	vim.bo.smartindent = true
-	vim.bo.tabstop = indent_size
-	vim.bo.softtabstop = indent_size
+	vim.bo.tabstop = settings.indent_size
+	vim.bo.softtabstop = settings.indent_size
 
 	-- Using Ctrl+C as esc
 	vim.api.nvim_set_keymap('i', '<C-c>', '<ESC>', { noremap = true })
